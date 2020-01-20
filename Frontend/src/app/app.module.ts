1feab7ec00113155c4fe29d18bf8a6ebc1e654b2
Zorbeus <artisja2@mymail.vcu.edu>
2020-01-20 04:01:54 -0500

    EPIC UPDATe But NOT FINIsheD

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchAreaComponent } from './search-area/search-area.component';
+import { SearchAreaResultComponent } from './search-area-result/search-area-result.component';
+import { FooterBarComponent } from './footer-bar/footer-bar.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchAreaComponent
+    SearchAreaComponent,
+    SearchAreaResultComponent,
+    FooterBarComponent,
+    HeaderBarComponent
   ],
   imports: [
     BrowserModule,
