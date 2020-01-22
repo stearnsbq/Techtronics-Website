0820f6338020f248aeeddb31cce2c5003aa8a17b
Quinn <stearnsbq@vcu.edu>
2020-01-21 22:39:02 -0500

    local changes

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModalComponent } from './login-modal/login-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/src/app/login-modal/login-modal.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { faUser, faLock, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-modal.component.html',
+  styleUrls: ['./login-modal.component.scss']
+})
+export class LoginModalComponent implements OnInit {
+  @ViewChild('Login', undefined) login_modal;
+  public submitted: boolean;
+
+  // icons for the login
+  public user = faUser;
+  public lock = faLock;
+  public login_icon = faUserCircle;
+
+
+  constructor() {
+    this.submitted = false;
+  }
+
+  ngOnInit() {
+
+  }
+
+
+  public onModalOpen() {
+    if (this.login_modal.nativeElement.style.display === 'none') {
+      this.login_modal.nativeElement.style.display = 'block';
+      this.login_modal.nativeElement.style['background-color'] = 'black';
+    } else {
+      this.login_modal.nativeElement.style.display = 'none';
+    }
+
+  }
+
+
+  // login event handler need to refactor as it is a little quirky doe
+
+  public onLogin(formData) {
+
+    this.submitted = true;
+
+    console.log(formData);
+
+  }
+
+
+
+}
