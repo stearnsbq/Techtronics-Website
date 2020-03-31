0318b3182eea7a6fbc598f2ccb9eeb567753f58d
Zorbeus <artisja2@mymail.vcu.edu>
2020-03-30 20:01:15 -0400

    remove side-bar crap

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
-import { SideNavMenuComponent } from './side-nav-menu/side-nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterBarComponent,
-    SideNavMenuComponent
+    FooterBarComponent
   ],
   imports: [
     BrowserModule,
