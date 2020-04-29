8c5effcd14b9f6dfce51d23c0fd5bc3671e634d6
Zorbeus <artisja2@mymail.vcu.edu>
2020-04-29 15:20:21 -0400

    removed nav shadow bar

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -48,7 +48,7 @@ export function tokenGetter() {
     EmployeepanelComponent,
     LoadingComponent,
     HomePageComponent,
-    RegistrationComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
