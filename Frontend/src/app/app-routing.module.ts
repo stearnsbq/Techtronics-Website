05f43cc21374f597f286218a8a5eaa19579bb786
Zorbeus <artisja2@mymail.vcu.edu>
2020-05-03 00:17:42 -0400

    changes concerning error with profile component removal

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ItemAreaComponent } from './item-area/item-area.component';
 import { ProductPageComponent } from './product-page/product-page.component';
-import { ProfileComponent } from './profile/profile.component';
-import { AuthGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard'; 
 import { RoleGuardService } from './role-guard.service'; 
 
 
